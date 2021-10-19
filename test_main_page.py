2a31ba4723fe972464f0a574d3260f8a180da7e9
Тёткин Александр <a.tyotkin@k-telecom.org>
2021-10-19 11:46:53 +0500

    Добавлено исключение

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from pages.main_page import MainPage
+from selenium.common.exceptions import NoSuchElementException
+
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,3 +8,16 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def is_element_present(self, how, what):
+    try:
+        self.browser.find_element(how, what)
+    except (NoSuchElementException):
+        return False
+    return True
